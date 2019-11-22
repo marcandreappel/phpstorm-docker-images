67df6d63613040869779228bd2f4fed3ff3645fd
Marc-André Appel <marc-andre@hybride-conseil.fr>
2019-11-22 17:06:48 +0100

    Started modifications

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,2 +1,2 @@
-CREATE USER 'jetbrains'@'%' IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON jetbrains.* TO 'jetbrains'@'%';
+CREATE USER 'root'@'%' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
